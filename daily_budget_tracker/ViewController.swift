f49a156565411f1e85806d7c22bdddf3ccddeca0
Minseo Cho <minseo.cho27@myhunter.cuny.edu>
2023-04-11 21:32:38 -0400

    First Commit
    Initial code for the project

---
+++ daily_budget_tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daily_budget_tracker
+//
+//  Created by Minseo Cho on 4/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
