a416be178ced0c820ffa1a5f85f77ecfe41bd0d6
Minseo Cho <minseo.cho27@myhunter.cuny.edu>
2023-04-30 23:09:00 -0400

    added camera that available on physical ios device

--- daily_budget_tracker/Purchase/PurchaseViewController.swift
+++ daily_budget_tracker/Purchase/PurchaseViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import PhotosUI
 import ParseSwift
 
-class PurchaseViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class PurchaseViewController: UIViewController{
     
     
     @IBOutlet weak var uploadButton: UIBarButtonItem!
@@ -109,11 +109,7 @@ class PurchaseViewController: UIViewController, UIImagePickerControllerDelegate 
         }
     }
     
-    
     @IBAction func onTakePhotoTapped(_ sender: Any) {
-        // TODO: Pt 2 - Present camera
-        // Make sure the user's camera is available
-        // NOTE: Camera only available on physical iOS device, not available on simulator.
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             print("❌📷 Camera not available")
             return
@@ -125,19 +121,13 @@ class PurchaseViewController: UIViewController, UIImagePickerControllerDelegate 
         // Shows the camera (vs the photo library)
         imagePicker.sourceType = .camera
 
-        // Allows user to edit image within image picker flow (i.e. crop, etc.)
-        // If you don't want to allow editing, you can leave out this line as the default value of `allowsEditing` is false
         imagePicker.allowsEditing = true
 
-        // The image picker (camera in this case) will return captured photos via it's delegate method to it's assigned delegate.
-        // Delegate assignee must conform and implement both `UIImagePickerControllerDelegate` and `UINavigationControllerDelegate`
         imagePicker.delegate = self
 
         // Present the image picker (camera)
         present(imagePicker, animated: true)
-
     }
-    
     @IBAction func onViewTapped(_ sender: Any) {
         view.endEditing(true)
     }
@@ -192,6 +182,23 @@ class PurchaseViewController: UIViewController, UIImagePickerControllerDelegate 
             }
         }
     }
+extension PurchaseViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 
+        // Dismiss the image picker
+        picker.dismiss(animated: true)
 
+        // Get the edited image from the info dictionary (if `allowsEditing = true` for image picker config).
+        // Alternatively, to get the original image, use the `.originalImage` InfoKey instead.
+        guard let image = info[.editedImage] as? UIImage else {
+            print("❌📷 Unable to get image")
+            return
+        }
 
+        // Set image on preview image view
+        previewImageView.image = image
+
+        // Set image to use when saving post
+        pickedImage = image
+    }
+}
